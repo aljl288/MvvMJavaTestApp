60471fad01642460917b31ba78986b884f8ba1ea
jiale.ee <alvin971028@gmail.com>
2022-07-05 12:55:52 +0800

    Add comment3

--- app/src/main/java/com/example/mvvmjava/viewmodel/AppViewModel.java
+++ app/src/main/java/com/example/mvvmjava/viewmodel/AppViewModel.java
@@ -23,7 +23,7 @@ public class AppViewModel extends BaseObservable {
     // string variable for
     // toast message
     // toast message
-    // hello branch1
+    // hello branch11
     private String toastMessage = null;
 
     // getter and setter methods
